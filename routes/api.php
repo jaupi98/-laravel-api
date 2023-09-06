ab71490fd9ce625ddd94000e1b5cfa475a5bafd3
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-09-06 22:52:33 +0200

    TypeCotroller

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TypeController;
+
 
 // use App\Http\Controllers\Api\ProjectController as ProjectController;
 
@@ -32,3 +34,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     ]);
 // });
 Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/types', [TypeController::class, 'index']);
